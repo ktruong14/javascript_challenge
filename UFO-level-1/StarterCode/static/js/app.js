829eb3e03aa33d76794fd0c1d4400ab00c99e5c3
ktruong14 <kev.truong14@gmail.com>
2020-11-30 20:56:54 +1100

    final

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -7,10 +7,15 @@ var tbody = d3.select("tbody");
 // Creating table
 function createTable(data) {
 
+  // Clearing existing data
   tbody.html("");
+  // Loop Through 'data' and each ufo sighting object
   data.forEach((ufoSightings) => {
+    // Using d3 to append one table row 'tr' for each ufo sighting object
     var row = tbody.append("tr");
+    // Use 'Object.entries' to console.log each ufo sighting value
     Object.entries(ufoSightings).forEach(([key, value]) => {
+      // Appending a cell to the row for each value in the ufo sightings object
       var cell = row.append("td");
       cell.text(value);
 
@@ -18,18 +23,24 @@ function createTable(data) {
   });
 };
 
+// Creating a function to filter data
 function runEnter() {
 
+  // Prevent the page from refreshing
   d3.event.preventDefault();
+  
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  console.log(inputValue);
+
+  // Defining variable filtered data
   var filteredData = tableData;
   
-  // Check if input is not empty then run a filter on date search results
+  // Check if date input is not empty then run a filter on date search results
   if (inputValue != "") {
     filteredData = filteredData.filter(date => date.datetime === inputValue)
   };
